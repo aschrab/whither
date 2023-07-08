b22b3d22577da5d1a6cca7c9b58b8e0fa100ff3d
Aaron Schrab <aaron@schrab.com>
2023-07-08 14:17:33 -0400

    Add weather controller and route to it

---
+++ app/controllers/weather_controller.rb
@@ -0,0 +1,10 @@
+class WeatherController < ApplicationController
+  def index
+    if params[:address]
+      location = Location.new params[:address]
+      @weather = Weather.new location
+    else
+      @weather = {}
+    end
+  end
+end
